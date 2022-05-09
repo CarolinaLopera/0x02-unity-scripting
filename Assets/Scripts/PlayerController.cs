2e67d5cec9188b697b3a6aae73e77d1337e67972
carolina <carolinalopera08@gmail.com>
2022-05-09 01:07:42 -0500

    Get coins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float speed = 500f;
+   public Rigidbody rb;
+   public float speed = 500f;
+   private int score = 0;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rb.AddForce(0, 0, 0);
+   // Update is called once per frame
+   void FixedUpdate()
+   {
+      // rb.AddForce(0, 0, 0);
 
-        if (Input.GetKey("w")) {
-           rb.AddForce(0, 0, speed * Time.deltaTime);
-        }
-        if (Input.GetKey("a")) {
-           rb.AddForce(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("s")) {
-           rb.AddForce(0, 0, -speed * Time.deltaTime);
-        }
-        if (Input.GetKey("d")) {
-           rb.AddForce(speed * Time.deltaTime, 0, 0);
-        }
-    }
+      if (Input.GetKey("w")) {
+         rb.AddForce(0, 0, speed * Time.deltaTime);
+      }
+      if (Input.GetKey("a")) {
+         rb.AddForce(-speed * Time.deltaTime, 0, 0);
+      }
+      if (Input.GetKey("s")) {
+         rb.AddForce(0, 0, -speed * Time.deltaTime);
+      }
+      if (Input.GetKey("d")) {
+         rb.AddForce(speed * Time.deltaTime, 0, 0);
+      }
+   }
+
+   void OnTriggerEnter(Collider other) {
+      if (other.gameObject.tag == "Pickup") {
+         this.score += 1;
+         Debug.Log($"Score: {score}");
+         Destroy(other.gameObject);
+      }
+   }
 }
